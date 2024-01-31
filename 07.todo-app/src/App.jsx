e6f65ec1dc5f369216fafe3b1fa6fe07d734ef2d
KananAmir <amirovknn@gmail.com>
2024-01-31 11:33:34 +0400

    added edit and completed functionalities

--- 07.todo-app/src/App.jsx
+++ 07.todo-app/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import "./App.css";
 import Form from "react-bootstrap/Form";
 import InputGroup from "react-bootstrap/InputGroup";
@@ -10,7 +10,6 @@ import ListGroup from "react-bootstrap/ListGroup";
 import { FaTrashCan } from "react-icons/fa6";
 import { FaRegEdit } from "react-icons/fa";
 import { v4 as uuidv4 } from "uuid";
-
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
@@ -19,10 +18,17 @@ function App() {
   );
   const [inputValue, setInputValue] = useState("");
   const [errorStatus, setErrorStatus] = useState(false);
+  const [editStatus, setEditStatus] = useState(false);
+  const [editId, setEditId] = useState(null);
+
+  const inputRef = useRef();
 
   const handleAddTodo = () => {
     if (inputValue) {
-      let updatedTodos = [...todos, { id: uuidv4(12), todoText: inputValue }];
+      let updatedTodos = [
+        ...todos,
+        { id: uuidv4(12), todoText: inputValue, completed: false },
+      ];
       setTodos(updatedTodos);
       console.log(todos);
       console.log(updatedTodos);
@@ -38,6 +44,45 @@ function App() {
     let updatedTodos = [...todos.filter((item) => item.id !== id)];
     setTodos(updatedTodos);
     localStorage.setItem("todos", JSON.stringify(updatedTodos));
+    setInputValue("");
+    setEditStatus(false);
+  };
+
+  const handleEditTodo = (id) => {
+    let editedTodo = todos.find((item) => item.id === id);
+    // console.log(editedTodo);
+    setInputValue(editedTodo.todoText);
+    setEditStatus(true);
+    setEditId(id);
+
+    // console.log(inputRef.current);
+    inputRef.current.focus();
+  };
+
+  const handleEditTodoRow = () => {
+    let editedTodoIndex = todos.findIndex((item) => item.id === editId);
+
+    if (editedTodoIndex !== -1) {
+      let updatedTodos = [...todos];
+      updatedTodos[editedTodoIndex].todoText = inputValue;
+      setTodos([...updatedTodos]);
+      localStorage.setItem("todos", JSON.stringify(updatedTodos));
+    }
+
+    setEditStatus(false);
+    setInputValue("");
+  };
+
+  const handleCompletedTodo = (checked, id) => {
+    let indx = todos.findIndex((item) => item.id === id);
+
+    // console.log(indx);
+    if (indx !== -1) {
+      let updatedTodos = [...todos];
+      updatedTodos[indx].completed = checked;
+      setTodos([...updatedTodos]);
+      localStorage.setItem("todos", JSON.stringify(updatedTodos));
+    }
   };
   return (
     <>
@@ -54,12 +99,17 @@ function App() {
                 }}
                 value={inputValue}
                 className={errorStatus && "border-1 border-danger"}
+                ref={inputRef}
               />
             </InputGroup>
             {errorStatus && <p className="text-danger">You should add todo!</p>}
           </Col>
           <Col xs={2} className="d-flex justify-content-end align-items-start">
-            <Button onClick={() => handleAddTodo()}>ADD</Button>
+            {!editStatus ? (
+              <Button onClick={() => handleAddTodo()}>ADD</Button>
+            ) : (
+              <Button onClick={() => handleEditTodoRow()}>Edit</Button>
+            )}
           </Col>
         </Row>
 
@@ -73,10 +123,25 @@ function App() {
                     variant="success"
                     className="mb-4 d-flex justify-content-between align-items-center"
                   >
-                    <span>{todo.todoText}</span>
+                    <div className="d-flex gap-2 align-items-center">
+                      <InputGroup.Checkbox
+                        checked={todo.completed}
+                        aria-label="Checkbox for following text input"
+                        onChange={(e) =>
+                          handleCompletedTodo(e.target.checked, todo.id)
+                        }
+                      />
+                      <span
+                        style={{
+                          textDecoration: todo.completed && "line-through",
+                        }}
+                      >
+                        {todo.todoText}
+                      </span>
+                    </div>
                     <div className="actions d-flex gap-3">
                       <FaTrashCan onClick={() => handleDeleteTodo(todo.id)} />
-                      <FaRegEdit />
+                      <FaRegEdit onClick={() => handleEditTodo(todo.id)} />
                     </div>
                   </ListGroup.Item>
                 );
